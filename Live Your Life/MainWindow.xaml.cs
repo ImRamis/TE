73513b83c01ba20ee5220225c85e268777947934
ImRamis <mr.iramis@hotmail.com>
2017-02-14 09:42:40 +0500

    with templates

--- Live Your Life/MainWindow.xaml.cs
+++ Live Your Life/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,40 +31,93 @@ namespace Live_Your_Life
                 System.Reflection.BindingFlags.GetProperty | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic,
                 null, this.Titanium, new object[] { });
             activeX.Silent = true;
-        } 
+        }
+        void TabControl(int tabNo)
+        {
+            for (int i = 0; i < tabs.Count; i++)
+                if (i == tabNo)
+                    tabs[i].Visibility = Visibility.Visible;
+                else
+                    tabs[i].Visibility = Visibility.Hidden;
+        }
+
         #endregion
+        List<Canvas> tabs = new List<Canvas>();
         public MainWindow()
         {
             InitializeComponent();
+            tabs.AddRange(new[] { MainMenu,WorkingSpace, CreationSpace, LoaderSpace });
+            foreach (var item in Directory.GetDirectories(AppDomain.CurrentDomain.BaseDirectory).Where(t => !t.Contains("Resources")))
+                folders.Items.Add(item.Split('\\').Last());
+            TabControl(0);
         }
+        int index = 0;
         void CreateNew(object sender, RoutedEventArgs e)
         {
-            meraBilla.Visibility = Visibility.Hidden;
-            teraBilla.Visibility = Visibility.Visible;
+            TabControl(2);
+            m.Close();
         }
-        void CancelCreate(object sender, RoutedEventArgs e)
-        {
-            meraBilla.Visibility = Visibility.Visible;
-            teraBilla.Visibility = Visibility.Hidden;
-        }
+        void CancelCreate(object sender, RoutedEventArgs e) => TabControl(0);
         void templateInfo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (Template != null)
+            {
                 Template.Source = new BitmapImage(new Uri("pack://siteoforigin:,,,/Resources/" + templateInfo.SelectedValue.ToString().Split(' ').Last() + ".png"));
+                index = templateInfo.SelectedIndex;
+            }
         }
-        void OpenProject(object sender, RoutedEventArgs e)
-        {
-
-            Titanium.Navigate("http://192.168.5.17:8100/");
-            SuppressErrors();
+        Process m = new Process();
+        void OpenProject(object sender, RoutedEventArgs e) {
+            p = Project.LoadProject(folders.SelectedValue.ToString());
+//            m.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            m.StartInfo.FileName = "cmd.exe";
+            m.StartInfo.Arguments = "/C " + String.Format("cd {0} && ionic serve -b -a -p 9100", p.Project);
+            TabControl(3);
+            new Thread(() =>
+            {
+                m.Start();
+                System.Threading.Thread.Sleep(8000);
+                Titanium.Dispatcher.Invoke(new Action(() => {
+                    TabControl(1);
+                    Titanium.Navigate("http://localhost:9100"); }));
+            }).Start();
         }
-        void Save(object sender, RoutedEventArgs e)
+        Module p = null;
+        void CreateProject(object sender, RoutedEventArgs e)
         {
+            if (!Directory.Exists(location.Text))
+            {
+                string text = location.Text;
+                TabControl(3);
+                new System.Threading.Thread(() => {
+                    Project.CreateProject(text, index == 0 ? Templates.blank : index == 1 ? Templates.sidemenu : Templates.tabs);
+                    LoaderSpace.Dispatcher.Invoke(new Action(() => {
+                        TabControl(1);
+                        p = Project.LoadProject(location.Text);
+                    }));
+                }).Start();
+            }
+            else
+                MessageBox.Show("Project with the same name exist!");
         }
         void Exit(object sender, RoutedEventArgs e)
         {
+            Environment.Exit(1);
         }
         void Simulate(object sender, RoutedEventArgs e)
+        {
+            if (p != null)
+            {
+                m.Close();
+ //               m.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                m.StartInfo.FileName = "cmd.exe";
+                m.StartInfo.Arguments = "/C " + String.Format("cd {0} && ionic serve -a -p 9100", p.Project);
+                m.Start();
+                m.WaitForExit();
+            }
+        }
+
+        private void pageAdder(object sender, RoutedEventArgs e)
         {
         }
     }
@@ -73,12 +129,25 @@ namespace Live_Your_Life
     }
     class Project
     {
-
         public static void CreateProject(string ProjectName, Templates template)
         {
-            
+            Process p = new Process();
+//            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = "/C "+String.Format("ionic start {0} {1}",ProjectName,template.ToString());
+            p.Start();
+            p.WaitForExit();
         }
-        public static void LoadProject(string ProjectName)
+        public static Module LoadProject(string ProjectName)
+        {
+            Module p = new Module() { Project = ProjectName };
+            return p;
+        }
+        public static void RunProject(string ProjectName)
+        {
+
+        }
+        public static void BuildProject(string ProjectName)
         {
 
         }
@@ -86,6 +155,7 @@ namespace Live_Your_Life
     class Module
     {
         public Routes routes { get; set; }
+        public string Project { get; set; }
 
     }
     class Routes
